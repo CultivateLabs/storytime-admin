042c57361db3d1dc9e370a100ca1a02d5116766a
David Van Der Beek <earlynovrock@gmail.com>
2015-03-04 16:25:06 -0600

    add storytime to method_missing patch

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -3,7 +3,9 @@ module Admin
     def method_missing method, *args, &block
       # puts "LOOKING FOR ROUTES #{method}"
       if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
-        if main_app.respond_to?(method)
+        if storytime.respond_to?(method)
+          storytime.send(method, *args)
+        elsif main_app.respond_to?(method)
           main_app.send(method, *args)
         else
           super
@@ -15,7 +17,7 @@ module Admin
 
     def respond_to?(method)
       if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
-        if main_app.respond_to?(method)
+        if storytime.respond_to?(method) || main_app.respond_to?(method)
           true
         else
           super
