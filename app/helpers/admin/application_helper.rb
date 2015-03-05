a104d3fe3718570b9f2450c6380edbf59392fa4b
David Van Der Beek <earlynovrock@gmail.com>
2015-03-05 09:28:58 -0600

    Do not reference storytime in method missing helper

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -3,9 +3,7 @@ module Admin
     def method_missing method, *args, &block
       # puts "LOOKING FOR ROUTES #{method}"
       if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
-        if storytime.respond_to?(method)
-          storytime.send(method, *args)
-        elsif main_app.respond_to?(method)
+        if main_app.respond_to?(method)
           main_app.send(method, *args)
         else
           super
@@ -17,7 +15,7 @@ module Admin
 
     def respond_to?(method)
       if method.to_s.end_with?('_path') or method.to_s.end_with?('_url')
-        if storytime.respond_to?(method) || main_app.respond_to?(method)
+        if main_app.respond_to?(method)
           true
         else
           super
