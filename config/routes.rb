335869320be2a4ed3185417860a22614eadb8b11
Ben Roesch <bcroesch@gmail.com>
2015-04-25 13:09:30 -0400

    Handle models that are in a module

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 StorytimeAdmin::Engine.routes.draw do
   StorytimeAdmin.models.each do |model|
-    resources model.tableize.to_sym
+    if model.include?("::")      
+      components = model.tableize.split("/")
+      resource_name = components.pop
+      namespace(components.join("/")){ resources resource_name }
+    else
+      resources model.tableize.to_sym
+    end
   end
 end
